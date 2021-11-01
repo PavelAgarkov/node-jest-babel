5bf6bb29982355f2d8ddf99e900374a41b6c7a03
pavel <Agarkov.P.A@sblogistica.ru>
2021-11-01 19:35:02 +0300

    nodemon live server !-g

--- fetch.js
+++ fetch.js
@@ -3,9 +3,8 @@ import fetch from "node-fetch";
 let a = fetch('http://localhost:3000')
     .then((response) => {
         return response.json();
-    }).
-then((data) => {
-    console.log(data);
-});
+    }).then((data) => {
+        console.log(data);
+    });
 
 console.log(a)
