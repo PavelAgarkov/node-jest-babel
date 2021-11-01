0eb51c33239c5e6fc7fa9597ecf76acdb83746de
pavel <Agarkov.P.A@sblogistica.ru>
2021-11-01 19:28:30 +0300

    nodemon live server

--- commands.md
+++ commands.md
@@ -1,1 +1,4 @@
 `npx jest` - запуск тестов(jest)
+
+`npm install -g nodemon`
+`nodemon server.js` - запуск в лайв режиме node.js
--- server.js
+++ server.js
@@ -5,8 +5,13 @@ let app = express(), port;
 app.get('/', (req, res) => {
     res.header({'Access-Control-Allow-Origin': '*'});
     res.json({ username: 'Flavio' });
+});
+
+app.get('/app', (req, res) => {
+    res.header({'Access-Control-Allow-Origin': '*'});
+    res.json({ username: 'lol' });
 })
 
 app.listen( port = 3000, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-})
+});
